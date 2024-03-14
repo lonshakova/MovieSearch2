ee5f9d90216cdbf8edf69438ab2a3b32155b165e
lonshakova <lonshakova.ae@edu.spbstu.ru>
2024-03-14 10:41:19 +0300

    1

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div >
+    <v-layout >
+      <v-app-bar color="#202020">
+        <v-toolbar-title class="logo">SearchMovie</v-toolbar-title> 
+        <div class="btns">
+          <v-btn class="btn" icon="home"><span class="material-symbols-outlined">
+            other_houses
+            </span></v-btn>  
+          <v-btn class="btn" icon="mdi-open-in-new"><span class="material-symbols-outlined">
+            bookmark
+            </span></v-btn>  
+          <v-btn class="btn" icon=""><span class="material-symbols-outlined">
+            hotel_class
+            </span></v-btn>
+        </div>
+      </v-app-bar>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .logo{
+      padding: 10;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      color:#21C2F8;
+      font-size: 40px;
+      font-weight: 900;
+      line-height: 1.5;
+  }
+  .btn{
+      margin: 5px;
+      height: 58px;
+      width: 58px;
+      background-color: #21C2F8;
+      border-radius: 90px;
+  }
+  .btn:hover{
+    min-width: 100px;
+  }
+  .material-symbols-outlined{
+    color:#202020;
+    font-size: 36px;
+  }
+</style>
---
+++ src/components/SeachMovie.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="search-movie">
+    <div class="search">
+      <v-text-field class="text" placeholder="Введите название фильма..." variant="plain"></v-text-field>
+      <span class="material-symbols-outlined" @click="console.log(2)">
+        search
+      </span>
+    </div>
+    <div class="select">
+      <v-select :items="items" item-title="name" variant="plain" placeholder="ФИЛЬТР"></v-select>
+  </div>
+</div>
+    
+</template>
+
+<script>
+
+export default {
+data(){
+  return {
+    items: [
+        { value: "title", name: "По названию" },
+        { value: "year", name: "По году" },
+        { value: "rating", name: "По рейтингу" },
+        { value: "timing", name: "По длительности" }
+      ],
+  }
+}
+};
+</script>
+
+<style scoped>
+.search-movie{
+  margin-top: 120px;
+  margin-left: 74px;
+  width: 1000px;
+  display: flex;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.text{
+  padding-left: 10px;
+}
+.search {
+  padding: 10px;
+  border-radius: 30px;
+  height: 45px;
+  width: 700px;
+  background-color: #F3F7F8;
+  border-radius: 30px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  font-size: 36px;
+}
+
+.material-symbols-outlined {
+  color: #BDC3C7;
+  font-size: 36px;
+  cursor: pointer;
+}
+
+.select {
+  margin-left: 10px;
+  padding-right: 0px;
+  padding-left: 20px;
+  height: 45px;
+  min-width: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+  color: #202020;
+  background-color: #F3F7F8;
+  border-radius: 30px;
+}
+</style>
