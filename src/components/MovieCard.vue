7efcf3525bfbe62cc4b1d24d36be4f437a974128
lonshakova <lonshakova.ae@edu.spbstu.ru>
2024-03-21 09:32:34 +0300

    data

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -4,16 +4,19 @@
     class="card-movie"
     :class="{ 'on-hover': isHovering }"
     v-bind="props">
-      <v-img src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" class="align-end"
+      <v-img src="https://st.kp.yandex.net/images/film_big/535341.jpg" class="align-end"
         gradient="to bottom, rgba(0,0,0,0),rgba(0,0,0,0), rgba(0,0,0,.8)" cover height="500">
-        <div class="title-and-marks">
-          <div class="title">Какое-то название какого-то фильма</div>
-          <div class="marks" v-if="isHovering">
-            <v-btn class="btn" icon="mdi-bookmark"></v-btn>
+        <div class="info">
+          <div class="title-and-description">
+            <div class="title">1+1</div>
+            <div class="description" v-if="isHovering">Аристократ на коляске нанимает в сиделки бывшего заключенного. Искрометная французская комедия с Омаром Си</div>
+          </div>
+          
+          <div class="btns">
+            <v-btn class="btn" v-if="isHovering" icon="mdi-bookmark"></v-btn>
             <v-btn class="btn" icon="mdi-star"></v-btn>
           </div>
         </div>
-        
       </v-img>
     </v-card>
   </v-hover>
@@ -36,7 +39,7 @@
   opacity: 0.8;
 }
 
-.title-and-marks{
+.info{
   width: 350px;
   height: 500px;
   display: flex;
@@ -46,19 +49,36 @@
   text-align: center;
 }
 
+.title-and-description{
+  color: #F3F7F8;
+  text-transform: uppercase;
+}
+
 .title{
-  width: 350px;
   font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 30px;
   font-weight: 100;
-  text-transform: uppercase;
-  color: #F3F7F8;
+
 }
-.marks{
+
+.description{
+  margin: 10px;
+  text-align: left;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 17px;
+  font-weight: 100;
+}
+
+.btns{
   margin-top: 12px;
-  padding-right: 20px;
   width: 350px;
   display: flex;
   justify-content: end;
+}
+
+.btn{
+  margin-right: 12px;
+  background-color: rgba(44, 62, 80, 0.5);
+  color:#F3F7F8;
 }
 </style>
